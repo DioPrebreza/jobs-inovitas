b2449f38cb53a58c9feb18d46238abf1a74338fa
Dionis Prebreza <52274063+DioPrebreza@users.noreply.github.com>
2023-02-16 13:54:25 +0100

    fixed and added egzon parts

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Services from "./Components/Services";
 import Slider from "./Components/Slider";
 import Woman from "./Components/Woman";
 import Map from "./Components/Map";
+import About from "./Components/About";
+import Parallax from "./Components/parallax";
+import Footer from "./Components/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <About />
+      <Parallax />
       <Slider />
       <Woman />
       <Achievements />
@@ -20,6 +25,7 @@ function App() {
       <JobsList />
       <ContactInfo />
       <Map />
+      <Footer />
     </div>
   );
 }
