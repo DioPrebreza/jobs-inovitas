898700ddfef0800263baf56d55d6999a6ee1d604
egzonkukaj <egzonkukaj@gmail.com>
2023-02-16 12:51:08 +0100

    added egzon parts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import './App.css'
+import About from './Components/About'
+import Parallax from './Components/parallax';
+import Footer from './Components/footer';
+
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className='App'>
+      <About />
+      <Parallax />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
